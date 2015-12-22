6a032dcd890835806962bb512540160ef69b8cbc
Blindpixels <cradexco@gmail.com>
2015-12-22 11:36:16 +0200

    3.Hafta
    octaves

--- frek.m
+++ frek.m
@@ -1,7 +1,7 @@
 function [ff] = frek(nota,oktav)
 
 % notalarý bir arraye at
-notalar = ['A','B','C','D','E','F','G','a','b','d','e','f'];
+notalar = ['Sus','Do','Dod', 'Re', 'Mi' ,'Mib' ,'Fa', 'Fad', 'Sol', 'Sold', 'La', 'Si', 'Sib'];
 
 % referans
 ref = 16.35;
@@ -9,13 +9,22 @@ ref = 16.35;
 % arrayýn boyutu
 nsize = length(notalar);
 
+% http://stackoverflow.com/questions/8590478/how-to-check-whether-an-argument-is-supplied-in-function-call
+
+if ~exist('oktav','var') || isempty(oktav)
+  oktav = 4;
+end
+
 % oktavlarý gez [0-8]
 for oIndex=0:8
     % parametre oktav gezerken denk gelirse
        if oIndex == oktav
            % notalarýn boyutu kadar for
            for j=1:nsize
+               % char -> karakter array'ýna çevir
+               % eðer dizinin içinde baktýðýmýz nota girilen ise.s
                if nota==char(notalar(j))
+                   % frekans formülü
                     ff=2^oIndex*(2^((j-1)/12)*ref)
                end  
             end
